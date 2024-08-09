35e53558193a95884bdfb4ae796ff90ac81bf6e3
Yuto Takano <moa17stock@gmail.com>
2024-08-09 23:30:40 +0100

    Use Custom build type on all OSes but only act on Windows

---
+++ README.md
@@ -0,0 +1,6 @@
+On Linux/OSX, SDL2 is found in the system package manager.
+On Linux, run: apt-get install libsdl2-dev
+On OSX, run: brew install sdl2
+
+For Windows, SDL2 development libraries are provided within the repository, and
+Setup.hs (run automatically during the build process) should find them.
--- Setup.hs
+++ Setup.hs
@@ -20,6 +20,10 @@ import Distribution.Simple.LocalBuildInfo
 import Distribution.Simple.Setup
     ( ConfigFlags
     )
+import Distribution.System
+    ( buildOS
+    , OS (Windows)
+    )
 import Data.Maybe
     ( fromJust
     )
@@ -30,20 +34,42 @@ import System.Directory
 main :: IO ()
 main = defaultMainWithHooks simpleUserHooks { confHook = sdlConfHook }
 
+-- | A hook that adds the SDL include and lib directories to the build info on
+-- Windows. This is necessary to link against the SDL library during compilation.
+--
+-- On MacOS and Linux, the SDL library can be installed using package managers,
+-- which puts them in a standard system-wide location that GHC will find
+-- automatically. However, on Windows, due to Haskell being distributed not
+-- natively but on top of MinGW and MSYS2, installing and using external
+-- libraries is a much more manual and error-prone process.
+--
+-- Instead, in this Game Jam Template repository, we've bundled the Windows
+-- build of the SDL development libraries with the project, so that we can avoid
+-- a system-wide installation of SDL. But because it's now in a non-standard
+-- location, we need to tell GHC where to find those libraries. We could specify
+-- the path in the .cabal or cabal.project files, but they only accept absolute
+-- paths and not relative ones. Which means it's unsuited for a template repo
+-- that might be built on many different machines. Thus, we chose to use a
+-- custom Setup.hs file, which runs during the build process and basically
+-- dynamically modifies the .cabal file to add absolute paths before GHC reads it.
 sdlConfHook :: (GenericPackageDescription, HookedBuildInfo) -> ConfigFlags -> IO LocalBuildInfo
-sdlConfHook (description, buildInfo) flags = do
-    localBuildInfo <- confHook simpleUserHooks (description, buildInfo) flags
-    let packageDescription = localPkgDescr localBuildInfo
-        library = fromJust $ library packageDescription
-        libraryBuildInfo = libBuildInfo library
-    dir <- getCurrentDirectory
-    return localBuildInfo {
-        localPkgDescr = packageDescription {
-            library = Just $ library {
-                libBuildInfo = libraryBuildInfo {
-                    includeDirs = (dir ++ "/sdl/include"):(includeDirs libraryBuildInfo),
-                    extraLibDirs = (dir ++ "/sdl/lib"):(extraLibDirs libraryBuildInfo)
+sdlConfHook (description, buildInfo) flags =
+    -- Only add the SDL include and lib directories on Windows
+    if buildOS /= Windows
+        then pure $ fromJust $ confHook simpleUserHooks (description, buildInfo) flags
+        else do
+            localBuildInfo <- confHook simpleUserHooks (description, buildInfo) flags
+            let packageDescription = localPkgDescr localBuildInfo
+                library = fromJust $ library packageDescription
+                libraryBuildInfo = libBuildInfo library
+            dir <- getCurrentDirectory
+            pure localBuildInfo {
+                localPkgDescr = packageDescription {
+                    library = Just $ library {
+                        libBuildInfo = libraryBuildInfo {
+                            includeDirs = (dir ++ "/sdl2_win_mingw/include"):(includeDirs libraryBuildInfo),
+                            extraLibDirs = (dir ++ "/sdl2_win_mingw/lib"):(extraLibDirs libraryBuildInfo)
+                        }
+                    }
                 }
             }
-        }
-    }
