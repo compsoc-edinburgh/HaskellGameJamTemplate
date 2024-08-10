c0f82403c95d9203baa3a0ad381a27cd069b6260
Yuto Takano <moa17stock@gmail.com>
2024-08-10 18:44:31 +0100

    Delete sdl2_win_mingw from repo, make setup script download it

--- win_setup.bat
+++ win_setup.bat
@@ -1,7 +1,45 @@
 @echo off
 
-rem Create a cabal.project.local file containing the path to the local SDL2 libraries
+rem Don't run if the sdl2_win_mingw folder or cabal.project.local already exists
+if exist sdl2_win_mingw (
+    echo [31mError: The 'sdl2_win_mingw' folder already exists.[0m
+    echo If you want to re-run the setup, delete the 'sdl2_win_mingw' folder and 'cabal.project.local' file and try again.
+    exit /b
+)
+if exist cabal.project.local (
+    echo [31mError: The 'cabal.project.local' file already exists.[0m
+    echo If you want to re-run the setup, delete the 'sdl2_win_mingw' folder and 'cabal.project.local' file and try again.
+    exit /b
+)
 
+rem Download the SDL2 2.30.6 libraries
+rem Use powershell, as curl may not be installed
+echo.[32minfo [0m Downloading SDL 2.30.6 libraries...
+powershell -Command "& {$ProgressPreference='SilentlyContinue'; Invoke-WebRequest https://github.com/libsdl-org/SDL/releases/download/release-2.30.6/SDL2-devel-2.30.6-mingw.zip -OutFile SDL2-devel-2.30.6-mingw.zip}"
+
+rem Extract the SDL2 libraries
+rem Use powershell, as tar may not be installed
+echo.[32minfo [0m Extracting SDL 2.30.6 libraries...
+powershell -Command "& {Add-Type -AssemblyName System.IO.Compression.FileSystem; [System.IO.Compression.ZipFile]::ExtractToDirectory('SDL2-devel-2.30.6-mingw.zip', 'SDL2-devel-2.30.6-mingw')}"
+
+rem Remove the zip file
+echo.[32minfo [0m Removing downloaded SDL2-devel-2.30.6-mingw.zip file...
+del SDL2-devel-2.30.6-mingw.zip
+
+rem Copy the bin, include and lib folders to the sdl2_win_mingw folder
+echo.[32minfo [0m Making new sdl2_win_mingw folder...
+mkdir sdl2_win_mingw
+echo.[32minfo [0m Copying SDL2 libraries to new sdl2_win_mingw folder...
+xcopy /E /Y SDL2-devel-2.30.6-mingw\SDL2-2.30.6\x86_64-w64-mingw32\bin sdl2_win_mingw\bin\ > nul
+xcopy /E /Y SDL2-devel-2.30.6-mingw\SDL2-2.30.6\x86_64-w64-mingw32\include sdl2_win_mingw\include\ > nul
+xcopy /E /Y SDL2-devel-2.30.6-mingw\SDL2-2.30.6\x86_64-w64-mingw32\lib sdl2_win_mingw\lib\ > nul
+
+rem Remove the extracted SDL2 folder
+echo.[32minfo [0m Removing extracted SDL2-devel-2.30.6-mingw folder...
+rmdir /S /Q SDL2-devel-2.30.6-mingw
+
+rem Create a cabal.project.local file containing the path to the local SDL2 libraries
+echo.[32minfo [0m Creating cabal.project.local file...
 (
 echo.package sdl2
 echo.    extra-lib-dirs: "%cd:\=\\%\\sdl2_win_mingw\\lib"
